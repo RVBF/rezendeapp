8d805f5cf9d3c93be543480d8d1e49f975550db0
Rafael Barros Ferreira <rafaelbferreira@rezendeconstrucao.com.br>
2019-10-26 12:52:11 -0300

    Fiz a lógica de resposta de checklist

--- assets/js/checklist/checklistExecucao.form.ctrl.js
+++ assets/js/checklist/checklistExecucao.form.ctrl.js
@@ -17,6 +17,8 @@
 		_this.idChecklist = window.location.href.split('#')[1].substring(1, url.length).split('/')[1];
 		_this.questionamentos = null;
 		_this.indiceQuestionamentos =0;
+		_this.dataLimite = '';
+		_this.horaLimite = '';
 
 		// Cria as opções de validação do formulário
 		var criarOpcoesValidacao = function criarOpcoesValidacao() {
@@ -74,13 +76,81 @@
 			);
 		};
 
-		_this.configurarBotoes = function configurarBotoes() {
+
+		_this.popularColaboradores  =  function popularColaboradores(valor = 0)
+		{
+			var sucesso = function (resposta) {
+				$("#responsavel").empty();
+
+				$.each(resposta.data, function(i ,item) {
+					$("#responsavel").append($('<option>', {
+						value: item.colaborador.id,
+						text: item.colaborador.nome  + ' ' + item.colaborador.sobrenome
+					}));
+				});
+
+				$('#responsavel').formSelect();
+			};
+
+			var erro = function(resposta)
+			{
+				var mensagem = jqXHR.responseText || 'Erro ao popular select de farmácias.';
+				toastr.error(mensagem);
+				return false;
+			}
+
+			var servicoUsuario = new app.ServicoUsuario();
+			var  jqXHR = servicoUsuario.todos();
+			jqXHR.done(sucesso).fail(erro);
+		};
+
+		_this.configurarEventos = function configurarEventos() {
+			_this.dataLimite =new Picker($('#data').get()[0], {
+				format : 'DD de MMMM de YYYY',
+				controls: true,
+				inline: true,
+				container: '.date-panel',					
+				text : {
+					title: 'Selecione a data',
+					cancel: 'Cancelar',
+					confirm: 'OK',
+					year: 'Ano',
+					month: 'Mês',
+					day: 'Dia',
+					hour: 'Hora',
+					minute: 'Minuto',
+					second: 'Segundo',
+					millisecond: 'Milissegundos',
+				},
+				headers : true,
+				months : ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+				monthsShort : ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+			});
+
+			_this.horaLimite = new Picker($('#hora').get()[0], {
+				format: 'HH:mm',
+				headers: true,
+				controls: true,
+				inline: true,
+				container: '.time-panel',	
+				text : {
+					title: 'Selecione a hora',
+					cancel: 'Cancelar',
+					confirm: 'OK',
+					hour: 'Hora',
+					minute: 'Minuto',
+					second: 'Segundo',
+					millisecond: 'Milissegundos',
+				},
+			});
+
 			_this.botaoSubmissao.on('click', _this.salvar);
 		};
 
 		_this.iniciarFormularioModoCadastro = function iniciarFormularioModoCadastro() {
+			_this.popularColaboradores();
 			_this.buscarQuestionamentos();
-			_this.configurarBotoes();
+			_this.configurarEventos();
 		};
 
 		_this.popularQuestao = function popularQuestao() {
@@ -89,103 +159,177 @@
 			html += '<div class="row form-row questionamento">'
 				html +='<div class="col col-sm-12 col-md-12 col-lg-12 col-12 mb-0-dto">';
 
-					html +='<div class="card-panel left-align pergunta">';
-						html += '<input type= "hidden" class="id"  name="questionamento_' + objetoAtual.id + '" value ="'+ objetoAtual.id  +'">';
+				html +='<div class="card-panel left-align pergunta">';
+					html += '<input type= "hidden" class="id"  name="questionamento_' + objetoAtual.id + '" value ="'+ objetoAtual.id  +'">';
 
-						html += '<div class="row form-row mb-0-dto">'
-							html +='<div class="col col-sm-12 col-md-12 col-lg-12 col-12 mb-0-dto">';
-								html +='<p class="mb-0-dto">';
-								html +='<strong class="fw-700-dto">'+(_this.indiceQuestionamentos+1)+'</strong> - ' + objetoAtual.formularioPergunta.pergunta;
-								html +='</p>';
-							html +='</div>';
+					html += '<div class="row form-row mb-0-dto">'
+						html +='<div class="col col-sm-12 col-md-12 col-lg-12 col-12 mb-0-dto">';
+							html +='<p class="mb-0-dto">';
+							html +='<strong class="fw-700-dto">'+(_this.indiceQuestionamentos+1)+'</strong> - ' + objetoAtual.formularioPergunta.pergunta;
+							html +='</p>';
 						html +='</div>';
-
-						html += '<div class="row form-row mb-0-dto">'
-							html +='<div class="col col-sm-4 col-md-4 col-lg-2 col-4 d-flex justify-content-center">';
-								html +='<input class="cb-dto opcao" type="radio" id="bom" name="opcao" value="bom">';
-								html +='<label class="label-dto" for="bom">';
-								html +='<i class="mdi mdi-emoticon-happy-outline large orange-text text-accent-4"></i>';
-								html +'</label>'
-							html +='</div>';
-
-							html +='<div class="col col-sm-4 col-md-4 col-lg-2 col-4 d-flex justify-content-center">';
-								html +='<input class="cb-dto regular opcao" type="radio" id="regular" name="opcao" value="regular">';
-								html +='<label class="label-dto" for="regular">';
-								html +='<i class="mdi mdi-emoticon-neutral-outline large orange-text text-accent-4"></i>';
-								html +'</label>'
-
-							html +='</div>';
-
-							html +='<div class="col col-sm-4 col-md-4 col-lg-2 col-4 d-flex justify-content-center">';
-								html +='<input class="cb-dto opcao" type="radio" id="ruim" name="opcao" value="ruim">';
-								html +='<label class="label-dto" for="ruim">';
-								html +='<i class="mdi mdi-emoticon-sad-outline large orange-text text-accent-4"></i>';
-								html +'</label>'
-							html +='</div>';
-						html +='</div>';
-
 					html +='</div>';
 
-					html += '<div class="opcoes_questionamento col col-sm-12 col-md-12 col-lg-12 col-12 center-align" style="display: none">';
-						html += '<div class="row form-row ">'
-							html += '<div class="col col-2 col-sm-2 col-lg-2 col-md-2 igs">';
-									html += '<div class="element orange accent-4 subicon-dto">';
-									html += '<i class="mdi mdi-information-outline white-text"></i></i><span class="name toltip" title="Nenhum arquivo selecionado.">Nenhum arquivo...</span>';
-									html += '<input type="file" name="pergunta_foto" id="pergunta_foto" accept="image/*">';
-									html += '</div>';
-			
-								// html += '<a class="orange accent-4 subicon-dto">';
-								// html += '<i class="mdi mdi-information-outline white-text"></i>';
-								// html += '</a>';
-							html += '</div>';
-							
-							html += '<div class="col col-2 col-sm-2 col-lg-2 col-md-2 igs">';
-									html += '<a class="orange accent-4 subicon-dto">';
-									html += '<i class="mdi mdi-microphone white-text"></i>';
-									html += '</a>';
-							html += '</div>';
-							
-							html += '<div class="col col-2 col-sm-2 col-lg-2 col-md-2 igs">';
-									html += '<a class="orange accent-4 subicon-dto">';
-									html += '<i class="mdi mdi-camera-outline white-text"></i>';
-									html += '</a>';
-							html += '</div>';
+					html += '<div class="row form-row mb-0-dto">'
+						html +='<div class="col col-sm-4 col-md-4 col-lg-2 col-4 d-flex justify-content-center justify-content-md-start">';
+							html +='<input class="cb-dto opcao" type="radio" id="bom" name="opcao" value="bom">';
+							html +='<label class="label-dto" for="bom">';
+							html +='<i class="mdi mdi-emoticon-happy-outline large orange-text text-accent-4"></i>';
+							html +'</label>'
+						html +='</div>';
 
-							html += '<div class="col col-2 col-sm-2 col-lg-2 col-md-2 igs">';
-								html += '<a class="orange accent-4 subicon-dto">';
-								html += '<i class="mdi mdi-lead-pencil white-text"></i>';
-								html += '</a>';
-							html += '</div>';
-							
-							html += '<div class="col col-2 col-sm-2 col-lg-2 col-md-2 igs">';
-								html += '<a href="pa-cadastro.html" class="orange accent-4 subicon-dto">';
-								html += '<span class="white-text">P.A.</span>';
-								html += '</a>';
-							html += '</div>';
+						html +='<div class="col col-sm-4 col-md-4 col-lg-2 col-4 d-flex justify-content-center justify-content-md-start">';
+							html +='<input class="cb-dto regular opcao" type="radio" id="regular" name="opcao" value="regular">';
+							html +='<label class="label-dto" for="regular">';
+							html +='<i class="mdi mdi-emoticon-neutral-outline large orange-text text-accent-4"></i>';
+							html +'</label>'
 
-							html += '<div class="col col-2 col-sm-2 col-lg-2 col-md-2 igs">';
-								html += '<a class="orange accent-4 subicon-dto">';
-								html += '<span class="white-text">Pend</span>';
-								html += '</a>';
+						html +='</div>';
+
+						html +='<div class="col col-sm-4 col-md-4 col-lg-2 col-4 d-flex justify-content-center justify-content-md-start">';
+							html +='<input class="cb-dto opcao" type="radio" id="ruim" name="opcao" value="ruim">';
+							html +='<label class="label-dto" for="ruim">';
+							html +='<i class="mdi mdi-emoticon-sad-outline large orange-text text-accent-4"></i>';
+							html +'</label>'
+						html +='</div>';
+					html +='</div>';
+
+				html +='</div>';
+
+				html += '<div class="opcoes_questionamento col col-sm-12 col-md-12 col-lg-12 col-12 center-align" style="display: none">';
+					html += '<div class="row form-row ">'
+						html += '<div class="list-group">';
+								html += '<div class="col col-4 col-sm-4 col-lg-2 col-md-2 igs">';
+										html += '<a class="list-group-item list-group-item-action orange accent-4 subicon-dto element">';
+										html += '<i class="mdi mdi-information-outline white-text"></i>';
+										html += '</a>';
+								html += '</div>';
+								
+								html += '<div class="col col-4 col-sm-4 col-lg-2 col-md-2 igs">';
+										html += '<a class="list-group-item list-group-item-action orange accent-4 subicon-dto element">';
+										html += '<i class="mdi mdi-microphone white-text"><span class="name tooltip" title="Nenhum arquivo selecionado.">Nenhum arquivo...</span></i>';
+										html += '<input type="file" name="pergunta_audio" id="pergunta_audio" accept="image/*">';
+										html += '</a>';
+								html += '</div>';
+								
+								html += '<div class="col col-4 col-sm-4 col-lg-2 col-md-2 igs">';
+										html += '<a class="list-group-item list-group-item-action orange accent-4 subicon-dto element">';
+										html += '<i class="mdi mdi-camera-outline white-text"></i>';
+										html += '<input type="file" name="pergunta_camera" id="pergunta_camera" accept="image/*">';
+										html += '</a>';
+								html += '</div>';
+						
+								html += '<div class="col col-4 col-sm-4 col-lg-2 col-md-2 igs">';
+									html += '<a class=" list-group-item list-group-item-action  orange accent-4 subicon-dto element active">';
+									html += '<i class="mdi mdi-lead-pencil white-text"></i>';
+									html += '</a>';
+								html += '</div>';
+								
+								html += '<div class="col col-4 col-sm-4 col-lg-2 col-md-2 igs">';
+									html += '<a href="#" class=" list-group-item list-group-item-action  orange accent-4 subicon-dto">';
+									html += '<span class="white-text">P.A.</span>';
+									html += '</a>';
+								html += '</div>';
+
+								html += '<div class="col col-4 col-sm-4 col-lg-2 col-md-2 igs">';
+									html += '<a href="#" class=" list-group-item list-group-item-action  orange accent-4 subicon-dto">';
+									html += '<span class="white-text">PEND</span>';
+									html += '</a>';
+								html += '</div>';
 							html += '</div>';
 						html += '</div>';
+
+
 					html += '</div>';
+				html += '</div>';
 			html += '</div>';
 			
 					
-			_this.formulario.find('.perguntas').empty().append(html);
+			_this.formulario.find('.perguntas').empty().append(html).promise().done(function () {
+				_this.formulario.find('input[type="file"]').change(function(evt){
+					var elemento = $(this);
+					var file = evt.target.files[0];
+					var reader = new FileReader();
+					var idPergunta = elemento.attr('name').split('_')[2];
+					
+					var nomeArquivo = $(this).val().split('\\');
+					nomeArquivo = nomeArquivo[nomeArquivo.length -1];
+
+					reader.readAsDataURL(file);
+
+					reader.onload = function () {
+						if(_this.respostas.length > 0){
+							var estaAdicionado = false;
+							for(var posicaoAtual in _this.respostas){
+								var atual = _this.respostas[posicaoAtual];
+								if(atual.pergunta == idPergunta){
+									atual.files.push({'nome': nomeArquivo, 'arquivo': reader.result, 'tipo' : file.type});
+									estaAdicionado = true;
+									_this.respostas[posicaoAtual] = atual;
+									break;
+								}
+							}
+							
+							if(!estaAdicionado) {
+								var resposta = new app.Resposta();
+								resposta.pergunta = idPergunta;
+								resposta.files.push({'nome': nomeArquivo, 'arquivo': reader.result, 'tipo' : file.type});
+								_this.respostas.push(resposta);
+							}
+						}
+						else{
+							
+							var resposta = new app.Resposta();
+							resposta.pergunta = idPergunta;
+							resposta.files.push({'nome': nomeArquivo,'arquivo': reader.result, 'tipo' : file.type});
+							_this.respostas.push(resposta);
+						}
+					};
+					reader.onerror = function (error) {
+					};
+				});
+
+	
+				_this.formulario.find('i').on('click', function () {
+					$(this).parents('.element').find("input[type='file']").trigger('click');
+				});
+		
+				_this.formulario.find('input[type="file"]').on('change', function() {
+					var val = $(this).val().split('\\');
+					val = val[val.length -1];
+
+					$(this).siblings('span').attr('data-original-title', val)
+					$(this).siblings('span').html(val.substring( 0, 12) + '...');
+				});
 
 
-			_this.formulario.find('input[type="radio"]').on('change',function(e){
-				if(this.value != "bom"){
-					_this.formulario.find('.opcoes_questionamento').show(100);
-					$('.modal').find('#nome-categoria').html(objetoAtual.checklist.titulo);
-					$('.modal').modal();
-				}else {
-					_this.formulario.find('.opcoes_questionamento').hide(100);
-				}
+				_this.formulario.find('.seleciona_resposta').on('click', function() {
+					var elemento = $(this);
+
+					if(elemento.hasClass('exige_planoacao')){
+						var id = parseInt(elemento.attr('name').split('_')[1]);
+						console.log(id);
+						var html = '<div class="vertical-divider"></div><div class="col-xs-6  col-sm-6 col-md-6 col-6 plano_acao">';
+						html += '<label for="descricao_planoacao_pergunta_' + id + '">Descrição</label>';
+						html += '<textarea class="form-control" rows="3" name="descricao_planoacao_pergunta_' + id + '" id="descricao_planoacao_pergunta_' + id + '" ></textarea>';
+						html += '</div>'
+
+						elemento.parents('.linha_atual').append(html);
+					}
+				});
+
+				_this.formulario.find('input[type="radio"]').on('change',function(e){
+					if(this.value != "bom"){
+						_this.formulario.find('.opcoes_questionamento').show(100);
+						$('.modal').find('#nome-categoria').html(objetoAtual.checklist.titulo);
+						$('.modal').modal();
+					}else {
+						_this.formulario.find('.opcoes_questionamento').hide(100);
+					}
+				});
+
 			});
-		}
+		};
 
 		_this.buscarQuestionamentos  =  function buscarQuestionamentos(valor = 0)
 		{
