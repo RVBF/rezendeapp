9144d32b6502ccd098621c29256d0063ff3b65e0
Leonardo Carvalhães Bernardo <leonardocarvalhaes@hotmail.com>
2019-12-26 18:30:19 -0300

    Criei o script que salva os recursos e rotas no banco de dados.

---
+++ api/scripts/salvarTodosOsRecursosERotas.php
@@ -0,0 +1,37 @@
+<?php
+require_once __DIR__.'/../bootstrap.php';
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+$debug = true;
+
+if($debug) echo PHP_EOL . 'Obtendo os dados de recursos e rotas...' . PHP_EOL;
+
+$recursosMatriz = Recurso::todosOsRecursosERotas();
+
+if($debug) echo PHP_EOL . 'Esvaziando as tabelas de recursos e rotas...' . PHP_EOL;
+
+DB::table('recurso')->truncate();
+DB::table('rota')->truncate();
+
+foreach($recursosMatriz as $recursoArray) {
+   if($debug) echo PHP_EOL . 'Salvando o recurso ' . $recursoArray['nome'] . ' (' . $recursoArray['model'] . ')...' . PHP_EOL;
+
+   $recurso = DB::table('recurso')->insert([
+      'id' => $recursoArray['id'],
+      'nome' => $recursoArray['nome'],
+      'model' => $recursoArray['model']
+   ]);
+
+   foreach($recursoArray['rotas'] as $rotaArray) {
+      if($debug) echo 'Salvando a rota ' . $rotaArray['caminho'] . ' (' . $rotaArray['metodo'] . ')...' . PHP_EOL;
+
+      $recurso = DB::table('rota')->insert([
+         'id' => $rotaArray['id'],
+         'caminho' => $rotaArray['caminho'],
+         'metodo' => $rotaArray['metodo'],
+         'recursoId' => $recursoArray['id']
+      ]);
+   }
+}
+?>
